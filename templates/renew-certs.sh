bf3fd6e22eefeff9639076fd35724856bcef7033
James Sumners <james@sumners.email>
2022-12-11 19:12:35 -0500

    Use static dns resolvers in cert operations

--- templates/renew-certs.sh
+++ templates/renew-certs.sh
@@ -18,36 +18,12 @@ lego --eab \
   --accept-tos \
   --path /root/.lego \
   --server https://acme.zerossl.com/v2/DV90 \
+  --dns.resolvers 1.1.1.1 \
   renew
 cat ${LEGO_CERTS}/{{ domain.cert_name }}.crt \
   ${LEGO_CERTS}/{{ domain.cert_name }}.key \
   > /opt/certs/{{ domain.cert_name }}.pem
 
 {% endfor %}
-
-lego --eab \
-  --kid "{{ zerossl_kid }}" \
-  --hmac "{{ zerossl_hmac }}" \
-  --domains "lt.smurf.pw" \
-  --domains "*.lt.smurf.pw" \
-  --http --http.port :9999 \
-  --dns gandiv5 \
-  --email "{{ zerossl_email }}" \
-  --accept-tos \
-  --path /root/.lego \
-  --server https://acme.zerossl.com/v2/DV90 \
-  renew
-
-lego --eab \
-  --kid "{{ zerossl_kid }}" \
-  --hmac "{{ zerossl_hmac }}" \
-  --domains "sats.smurf.pw" \
-  --http --http.port :9999 \
-  --dns gandiv5 \
-  --email "{{ zerossl_email }}" \
-  --accept-tos \
-  --path /root/.lego \
-  --server https://acme.zerossl.com/v2/DV90 \
-  renew
 
 sv reload haproxy
