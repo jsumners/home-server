ad6e6831262851b13c37829ea6061718601a44db
James Sumners <git@zifbang.com>
2024-03-31 17:40:26 -0400

    update udm-pro-api-client

--- templates/restart-access-point.sh
+++ templates/restart-access-point.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export API_CLIENT_CONFIG_FILE=/etc/udm-pro-api-client.yaml
-/usr/local/bin/udm-pro-api-client get-hosts > /opt/hosts.d/udm-pro
+/usr/local/bin/udm-pro-api-client device restart {{ u6_enterprise_mac }}
